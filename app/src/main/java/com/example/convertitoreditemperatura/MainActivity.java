5b316fe06921981188d820690307a835a45915a2
Alessio Cottarelli <a.cottarelli1@campus.unimib.it>
2020-03-23 15:57:57 +0100

    Gestita modalità full screen

--- app/src/main/java/com/example/convertitoreditemperatura/MainActivity.java
+++ app/src/main/java/com/example/convertitoreditemperatura/MainActivity.java
@@ -34,9 +34,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final int flags;
         super.onCreate(savedInstanceState);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        Objects.requireNonNull(getSupportActionBar()).hide();
+        flags =  View.SYSTEM_UI_FLAG_FULLSCREEN
+                |View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                |View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                |View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(flags);
         setContentView(R.layout.activity_main_constraint);
 
         init();
